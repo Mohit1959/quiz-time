7ad80da15a5b35de603ed02eb992ca86597edbc1
Mohit1959 <59610751+Mohit1959@users.noreply.github.com>
2021-05-11 15:32:57 +0530

    Add files via upload

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,42 @@
+export const Questions = [
+    {
+        prompt: "Which is the capital of India?",
+        optionA: "Kolkata",
+        optionB: "Banglore",
+        optionC: "New Delhi",
+        optionD: "Mumbai",
+        answer: "C"
+    },
+    {
+        prompt: "Thanos is a comic character which is related to:",
+        optionA: "DC Comics",
+        optionB: "Marvel Cimematic Universe",
+        optionC: "Walt Disney",
+        optionD: "None of these",
+        answer: "B"
+    },
+    {
+        prompt: "Who was the second Prime Minister of India?",
+        optionA: "Jawaharlal Nehru",
+        optionB: "Dr Rajaendra Prasad",
+        optionC: "Sardar Vallabh Bhai Patel",
+        optionD: "Lal Bahadur Shastri",
+        answer: "D"
+    },
+    {
+        prompt: "Hydrogen bomb is works on the principle of",
+        optionA: "Nuclear Fusion",
+        optionB: "Beta decay",
+        optionC: "Alpha decay",
+        optionD: "Nuclear Fision",
+        answer: "A"
+    },
+    {
+        prompt: "What is the virus called which causes the COVID-19",
+        optionA: "MERS-Cov",
+        optionB: "SARS-Cov-2",
+        optionC: "SARS-Cov-1",
+        optionD: "Ebola",
+        answer: "B"
+    }
+]
